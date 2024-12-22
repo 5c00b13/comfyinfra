02561111519cc0987d9e658371afc9ceefc71ad9
Jan Strydom <jnstrdm05@gmail.com>
2024-12-22 00:24:10 -0500

    yoo

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-  
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': new URL('./src', import.meta.url).pathname,
-    },
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_COOLIFY_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        secure: false,
-      },
-    },
   },
 });
