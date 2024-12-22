5e88817adda479aaee62f0b387f31814774922f4
Jan Strydom <jnstrdm05@gmail.com>
2024-12-21 22:03:37 -0500

    yoo

--- src/components/InfrastructureCanvas/NodeToolbar/index.tsx
+++ src/components/InfrastructureCanvas/NodeToolbar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ServiceButton } from './ServiceButton';
 import { cn } from '../../../lib/utils';
 import { CONFIG } from '../../../lib/config';
