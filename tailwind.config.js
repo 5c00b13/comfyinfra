dd36713da38887378108c96cce7a506f1ed9d796
Jan Strydom <jnstrdm05@gmail.com>
2024-12-24 01:35:31 -0500

    yoo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        background: '#0F172A',
-        foreground: '#E2E8F0',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-  darkMode: 'class',
-};
+  plugins: [
+    require('tailwind-scrollbar')({ nocompatible: true }),
+  ],
+}
